699f8392327424a32a505366d83afdcc0b3298e9
unknown <ibrahimyyildirim@github.com>
2018-12-11 21:58:16 +0300

    deneme

---
+++ classes.js
@@ -0,0 +1,11 @@
+class Customer {
+constructor(movieName,name,date,session,seat,price){
+    this.movieName=movieName
+    this.name=name
+    this.date=date
+    this.session=session
+    this.seat=seat
+    this.price=price
+    sfdsf
+    }
+}
